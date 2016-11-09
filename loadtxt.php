a32d4463711d4d3ec23bcba46309f4b017d94375
Dasl0ki <theloki1712@gmail.com>
2016-11-09 11:35:47 +0100

    Added time calculation

--- loadtxt.php
+++ loadtxt.php
@@ -1,4 +1,12 @@
 <?php
+	function dtm(\DateTime $dateTime)
+	{
+		$secs = $dateTime->getTimestamp(); // Gets the seconds
+		$millisecs = $secs*1000; // Converted to milliseconds
+		$millisecs += $dateTime->format("u")/1000; // Microseconds converted to seconds
+		return $millisecs;
+	}
+	
 	$file = fopen("ranking.txt", "r") or die("Error!");
 	if(file_exists('stop-script')) {
 		echo 'Status: Stopped<br>';
@@ -7,7 +15,19 @@
 	}
 	echo "Ranking:<br>";
 	while(!feof($file)) {
-		echo substr(fgets($file),3)."<br>";
+		$arr = explode("|", substr(fgets($file),3));
+		$team = $arr[0];
+		$date = $arr[1];
+		if(substr($team,0,1) == "1") {
+			$master_date = new DateTime($date);
+		} else {
+			$second_date = new DateTime($date);
+		}
+		if(substr($team,0,1) >= "2") {
+			$echo = number_format(Round((dtm($second_date)-dtm($master_date))/1000,3),3);
+		}
+		//echo substr(fgets($file),3,-28)."<br>";
+		echo "<br>".$team/*." ".$date*/; if(isset($echo)) {echo " +".$echo."s"; }
 	}
-	fclose($file);
+	fclose($file);	
 ?>
